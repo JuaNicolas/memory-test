f5ff52a02ccb5df65a5318ec077d7165796402e0
Nicolas Juanico <nicolas.juany@gmail.com>
2022-07-07 17:51:41 +0200

    Memory test init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
