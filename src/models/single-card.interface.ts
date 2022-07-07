d56a03f1df1bc6e6625049554ea17ad247cde824
Nicolas Juanico <nicolas.juany@gmail.com>
2022-07-07 21:17:41 +0200

    update interface

--- src/models/single-card.interface.ts
+++ src/models/single-card.interface.ts
@@ -4,5 +4,6 @@ import { Choice } from "./choice.interface";
 export interface SingleCardProps {
   card: Card;
   flipped: boolean;
+  disabled: boolean;
   handleChoice: (choice: Choice) => void;
 }
