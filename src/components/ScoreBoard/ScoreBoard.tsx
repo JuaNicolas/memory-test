de72415e34f3f2724055adefaf3671c6aa01a033
Nicolas Juanico <nicolas.juany@gmail.com>
2022-07-11 11:17:41 +0200

    Replace hooks with adapter fn

--- src/components/ScoreBoard/ScoreBoard.tsx
+++ src/components/ScoreBoard/ScoreBoard.tsx
@@ -1,23 +1,22 @@
-import { useEffect, useState } from "react";
 import { ScoreBoardProps } from "../../models/score-board.interface";
 import "./ScoreBoard.scss";
-function ScoreBoard({ scores, shuffleCards, turns }: ScoreBoardProps) {
-  const [score, setScore] = useState<number[]>([]);
+const formatScore = (scores: number): number[] => {
+  const scoresAsString = `${scores}`;
+  const score: number[] = Array(6 - scoresAsString.length).fill(0);
+  for (const n of scoresAsString) {
+    score.push(+n);
+  }
+  return score;
+};
 
-  useEffect(() => {
-    const scoresAsString = `${scores}`;
-    const score: number[] = Array(5 - scoresAsString.length).fill(0);
-    for (const n of scoresAsString) {
-      score.push(+n);
-    }
-    setScore(score);
-  }, [scores]);
+function ScoreBoard({ scores, shuffleCards, turns }: ScoreBoardProps) {
+  const formattedScore = formatScore(scores);
 
   return (
     <div className="highscores-grid">
       <button onClick={shuffleCards}>New Game</button>
       <p className="scores">
-        Score <span>{score}</span>
+        Score <span>{formattedScore}</span>
       </p>
       <p className="turns">Turns: {turns}</p>
     </div>
